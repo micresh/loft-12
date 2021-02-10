fbb97120112a657fbf85cb0193934e8e616be8d3
Mikhail Reshetnyak <micresh@gmail.com>
2021-02-10 11:29:09 +0300

    Update loft-2.py

--- loft-2.py
+++ loft-2.py
@@ -10,6 +10,7 @@ events_years = soup.find_all('span', class_='say-no-more')
 events_location = soup.find_all('span', class_='event-location')
 
 for x in range(len(events_names)):
+	# todo rewrite to formatted string or create iterator
 	s = events_names[x].text + ' ' + events_dates[x].text + ' ' + events_years[x].text + ' ' + events_location[x].text 
 	events.append(s)
 	
