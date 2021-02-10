b5c5ef415497faffccdd474fd942ad98eb33bc69
Mikhail Reshetnyak <micresh@gmail.com>
2021-02-10 11:49:02 +0300

    Update loft-2.py

--- loft-2.py
+++ loft-2.py
@@ -1,17 +1,17 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-resp = requests.get('https://python.org/events/')
-soup = bs(resp.text, 'lxml')
-events = []
-events_names = soup.find_all('h3', class_='event-title')
-events_dates = soup.find_all('time')
-events_years = soup.find_all('span', class_='say-no-more')
-events_location = soup.find_all('span', class_='event-location')
+resp = requests.get('https://python.org/events/') # get target web-page
+soup = bs(resp.text, 'lxml') # converting to soup for easily navigation in tags
+events = [] # init events_list
+events_names = soup.find_all('h3', class_='event-title') # get events names
+events_dates = soup.find_all('time') # get events dates, e.g. days and month
+events_years = soup.find_all('span', class_='say-no-more') # get events years
+events_location = soup.find_all('span', class_='event-location') # get events location
 
 for x in range(len(events_names)):
 	# todo rewrite to formatted string or create iterator
-	s = events_names[x].text + ' ' + events_dates[x].text + ' ' + events_years[x].text + ' ' + events_location[x].text 
+	s = events_names[x].text + ' ' + events_dates[x].text + ' ' + events_years[x].text + ' ' + events_location[x].text # generate final view of data
 	events.append(s)
 	
 for c in events:
